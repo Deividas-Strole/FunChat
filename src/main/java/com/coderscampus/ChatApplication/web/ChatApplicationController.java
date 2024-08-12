7de3d7b662b0232b919c65a8a602cc9d282f9722
deividas strole <deiwis@yahoo.com>
2024-08-11 17:58:29 -0700

    code clean up

--- src/main/java/com/coderscampus/ChatApplication/web/ChatApplicationController.java
+++ src/main/java/com/coderscampus/ChatApplication/web/ChatApplicationController.java
@@ -20,27 +20,10 @@ public class ChatApplicationController {
     @Autowired
     MessageService messageService;
 
-
-    //String yourName;
-
     @GetMapping("/welcome")
     private String welcome() {
         return "welcome";
     }
-
-//    @PostMapping("/channels")
-//    private String channels(@RequestBody User user, ModelMap model) {
-//        yourName = user.getName();
-//        System.out.println("yourName in PostMapping (1st func) == " + yourName);
-//        return "redirect:/channels";
-//    }
-//
-//    @GetMapping("/channels")
-//    private String getChannels(ModelMap model) {
-//        System.out.println("yourName in GetMapping (2nf func) == " + "yourName");
-//        model.put("name", "yourName");
-//        return "channels";
-//    }
 
     @GetMapping("/channels")
     @ResponseBody
@@ -53,29 +36,15 @@ public class ChatApplicationController {
     @PostMapping("/channels")
     private String channels(@RequestBody User user, ModelMap model) {
         String yourName = user.getName();
-        System.out.println("yourName in Controller (PostMaping - channels) " + yourName);
         model.put("name", yourName);
-
         return "/channels";
     }
 
     @PostMapping("/channels2")
     @ResponseBody
     private ResponseEntity cannels2(@RequestBody Message message, ModelMap model) {
-        System.out.println("****** we are in channels2!!!!");
-        System.out.println("Channels2 Textas: " + message.getMessageText());
-        System.out.println("Channels2 Name:" + message.getMessageUser());
-        //System.out.println("message list in controller before sending back to frontend: " + messageService.saveMessage(message));
-
-//************************* start here *** correct passing data from back end to front end ******* D O M
         List<Message>  listOfMessages = messageService.saveMessage(message);
         model.put("listOfMessages", listOfMessages);
         return ResponseEntity.ok().body(listOfMessages);
     }
-
-    @GetMapping("/test")
-    private String test() {
-        return "test";
-    }
-
 }
