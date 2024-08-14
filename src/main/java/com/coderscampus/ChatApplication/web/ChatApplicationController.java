f65e2930db0e6066359ae7cce23e9b6573c91495
deividas strole <deiwis@yahoo.com>
2024-08-13 17:33:12 -0700

    still working on improving welcome.js

--- src/main/java/com/coderscampus/ChatApplication/web/ChatApplicationController.java
+++ src/main/java/com/coderscampus/ChatApplication/web/ChatApplicationController.java
@@ -3,6 +3,8 @@ package com.coderscampus.ChatApplication.web;
 import com.coderscampus.ChatApplication.domain.Message;
 import com.coderscampus.ChatApplication.domain.User;
 import com.coderscampus.ChatApplication.service.MessageService;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
@@ -11,7 +13,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.view.RedirectView;
 
+import java.io.IOException;
 import java.util.List;
 
 @Controller
@@ -20,24 +24,61 @@ public class ChatApplicationController {
     @Autowired
     MessageService messageService;
 
+    @Autowired
+    private HttpSession session;
+
     @GetMapping("/welcome")
     private String welcome() {
         return "welcome";
     }
 
-    @GetMapping("/channels")
-    @ResponseBody
-    private ResponseEntity returnAllMessages(ModelMap model) {
-        List<Message>  listOfMessages = messageService.getAllMessage();
-        return ResponseEntity.ok().body(listOfMessages);
-    }
+//    @GetMapping("/channels")
+//    @ResponseBody
+//    private ResponseEntity returnAllMessages(ModelMap model) {
+//        List<Message>  listOfMessages = messageService.getAllMessage();
+//        return ResponseEntity.ok().body(listOfMessages);
+//    }
+
+//    @GetMapping("/channelsPage")
+//    public String channelsPage(ModelMap model) {
+//        // This would return a view named "channels"
+//        System.out.println("You are in channelsPage method");
+//        return "channels";
+//    }
+
+
+//    @PostMapping("/channels")
+//    private String  channels(@RequestBody User user, ModelMap model) {
+//        String yourName = user.getName();
+//        model.put("name", yourName);
+//        //return "redirect/test";
+//        //return "redirect:/channels";
+//        //return new RedirectView("/channels");
+//        //return "channels";
+//        return "redirect:/channelsPage";
+//    }
+
+//    @GetMapping("/test")
+//    private String test() {
+//        System.out.println("you are in test method");
+//        return "redirect:/channels";
+//    }
 
     @PostMapping("/channels")
-    private String channels(@RequestBody User user, ModelMap model) {
-        String yourName = user.getName();
-        model.put("name", yourName);
-        return "/channels";
+    public String channelsPost(@RequestBody User user) {
+        session.setAttribute("userName", user.getName());
+        return "redirect:/channels";
     }
+
+    @GetMapping("/channels")
+    public String channelsGet(ModelMap model) {
+        String userName = (String) session.getAttribute("userName");
+        model.addAttribute("name", userName);
+        List<Message> messages = messageService.getAllMessage();
+        model.addAttribute("messages", messages);
+        return "channels";
+    }
+
 
     @PostMapping("/channels2")
     @ResponseBody
--- src/main/resources/static/js/welcome.js
+++ src/main/resources/static/js/welcome.js
@@ -2,36 +2,56 @@ const name = prompt("Hello! :) What is your name?");
 
 const link = document.getElementById('myLink');
 
-const sendData = async () => {
+//const sendData = async () => {
+//
+//  const data = {name: name};
+//
+//  try {
+//
+//    const response = await fetch('http://localhost:8080/channels', {
+//      method: 'POST',
+//      headers: { 'Content-Type': 'application/json' },
+//      body: JSON.stringify(data)
+//    });
+//
+//    if (!response.ok) {
+//      throw new Error('Network response was not ok');
+//    }
+//
+//    // The response is now HTML, so we'll load it into the document
+//    const htmlContent = await response.text();
+//    document.open();
+//    document.write(htmlContent);
+//    document.close();
+//  }
+//
+//    catch (error) {
+//    console.error('Error:', error);
+//  }
+//
+//};
 
-  const data = {name: name};
 
-  try {
+function submitUser() {
 
-    const response = await fetch('http://localhost:8080/channels', {
-      method: 'POST',
-      headers: { 'Content-Type': 'application/json' },
-      body: JSON.stringify(data)
+    const data = {name: name};
+    
+    fetch('http://localhost:8080/channels', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ name: document.getElementById('nameInput').value }),
+    })
+    .then(response => response.json())
+    .then(data => {
+        // Assuming the server returns some data to confirm success
+        window.location.href = '/channels';
     });
-
-    if (!response.ok) {
-      throw new Error('Network response was not ok');
-    }
-
-    // The response is now HTML, so we'll load it into the document
-    const htmlContent = await response.text();
-    document.open();
-    document.write(htmlContent);
-    document.close();
-  }
-
-    catch (error) {
-    console.error('Error:', error);
-  }
-
-};
+}
 
 link.addEventListener("click", () => {
     event.preventDefault(); // Prevent default link behavior
-    sendData();
+//    sendData();
+    submitUser();
 });
