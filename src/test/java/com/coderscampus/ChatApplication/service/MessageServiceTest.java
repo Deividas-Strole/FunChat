021a1cb51c5f3f60b765b958b2184775d007a4fa
deividas strole <deiwis@yahoo.com>
2024-08-16 21:02:13 -0700

    created  getAllMessagesTest()

--- src/test/java/com/coderscampus/ChatApplication/service/MessageServiceTest.java
+++ src/test/java/com/coderscampus/ChatApplication/service/MessageServiceTest.java
@@ -1,38 +1,41 @@
 package com.coderscampus.ChatApplication.service;
 
 import com.coderscampus.ChatApplication.domain.Message;
-import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-
 import java.util.ArrayList;
 import java.util.List;
-
 import static org.junit.jupiter.api.Assertions.*;
 
 class MessageServiceTest {
 
-    @Test
-    void saveMessageTest() {
-        Message message = new Message();
+    Message message = new Message();
+    Message  expectedResultMessage = new Message();
+    MessageService messageService = new MessageService();
+    List<Message> actualResult;
+    List<Message> expectedResult = new ArrayList<>();
+
+    @BeforeEach
+    void prepData() {
+
         message.setMessageUser("TestUser");
         message.setMessageText("Test Text");
+        actualResult = messageService.saveMessage(message);
 
-        MessageService messageService = new MessageService();
-
-        List<Message> actualResult = messageService.saveMessage(message);
-
-        Message  expectedResultMessage = new Message();
         expectedResultMessage.setMessageUser("TestUser");
         expectedResultMessage.setMessageText("Test Text");
 
-        List<Message> expectedResult = new ArrayList<>();
         expectedResult.add(expectedResultMessage);
+    }
 
+
+        @Test
+    void saveMessageTest() {
         assertEquals(expectedResult, actualResult);
     }
 
     @Test
     void getAllMessages() {
+        assertEquals(expectedResult, actualResult);
     }
 }
