6c1935e837c2d29b5c21e9d3a3930b715ac78adc
deividas strole <deiwis@yahoo.com>
2024-08-16 20:46:34 -0700

    created saveMessageTest()

--- src/test/java/com/coderscampus/ChatApplication/service/MessageServiceTest.java
+++ src/test/java/com/coderscampus/ChatApplication/service/MessageServiceTest.java
@@ -1,25 +1,35 @@
 package com.coderscampus.ChatApplication.service;
 
+import com.coderscampus.ChatApplication.domain.Message;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class MessageServiceTest {
 
-    // WORK IN PROGRESS
-
-    @BeforeEach
-    void setUp() {
-    }
-
-    @AfterEach
-    void tearDown() {
-    }
-
     @Test
-    void saveMessage() {
+    void saveMessageTest() {
+        Message message = new Message();
+        message.setMessageUser("TestUser");
+        message.setMessageText("Test Text");
+
+        MessageService messageService = new MessageService();
+
+        List<Message> actualResult = messageService.saveMessage(message);
+
+        Message  expectedResultMessage = new Message();
+        expectedResultMessage.setMessageUser("TestUser");
+        expectedResultMessage.setMessageText("Test Text");
+
+        List<Message> expectedResult = new ArrayList<>();
+        expectedResult.add(expectedResultMessage);
+
+        assertEquals(expectedResult, actualResult);
     }
 
     @Test
