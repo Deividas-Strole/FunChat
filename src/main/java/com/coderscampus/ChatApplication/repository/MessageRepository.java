a5cf9035383db9499243c5bc7526b7d1724f6d69
deividas strole <deiwis@yahoo.com>
2024-11-11 14:31:17 -0800

    finished fixing multiple channel issue

--- src/main/java/com/coderscampus/ChatApplication/repository/MessageRepository.java
+++ src/main/java/com/coderscampus/ChatApplication/repository/MessageRepository.java
@@ -6,41 +6,20 @@ import org.springframework.stereotype.Repository;
 import java.util.ArrayList;
 import java.util.HashMap;
 
+@Repository
 public class MessageRepository {
 
-    ArrayList<Message> listOfMessages = new ArrayList<Message>();
-    HashMap<String, ArrayList<Message>> chanellMessages = new HashMap<String, ArrayList<Message>>();
+    private final HashMap<String, ArrayList<Message>> channelMessages = new HashMap<>();
 
     public void addMessageToArray(Message message, String channel) {
-        //chanellMessages.get(channel);
-        //listOfMessages.add(message);
-
-        //chanellMessages.put(channel, "green");
-
-        if (chanellMessages.containsKey(channel)) {
-            chanellMessages.get(channel).add(message);
-        } else {
-            listOfMessages.add(message);
-            chanellMessages.put(channel, listOfMessages);
-        }
+        channelMessages.computeIfAbsent(channel, k -> new ArrayList<>()).add(message);
 
         System.out.println("message user in repo: " + message.getMessageUser());
         System.out.println("message message in repo: " + message.getMessageText());
         System.out.println("channel in repo: " + channel);
-
-        //chanellMessages.put(channel, message);
-
-        //chanellMessages.get(channel).add(message);
-
     }
 
     public ArrayList<Message> listOfMessagesByChannel(String channel) {
-
-        if (chanellMessages.containsKey(channel)) {
-            return chanellMessages.get(channel);
-        } else {
-            return listOfMessages;
-        }
-        //return  chanellMessages.get(channel);
+        return channelMessages.getOrDefault(channel, new ArrayList<>());
     }
 }
